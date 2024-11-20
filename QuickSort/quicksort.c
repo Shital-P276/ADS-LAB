8a31134639368ad015a242ca86d63db2dc46c0ca
Shital-P276 <shital.kitcoek@gmail.com>
2024-11-20 22:44:20 +0530

    Update quicksort.c

--- QuickSort/quicksort.c
+++ QuickSort/quicksort.c
@@ -1,60 +1,68 @@
 #include <stdio.h>
 
-// Quick sort function declaration
 void qsort(int b[], int left, int right);
 
-int n;
-
-void main() {
-    int a[100], i, l, r;
-
+int main()
+{
+    int a[100], i, l, r, n;
+    
     printf("\nENTER THE SIZE OF THE ARRAY: ");
     scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
+    
+    for (i = 0; i < n; i++)
+    {
         printf("\nENTER NUMBER-%d: ", i + 1);
         scanf("%d", &a[i]);
     }
-
+    
     printf("\nTHE ARRAY ELEMENTS BEFORE SORTING: \n");
-    for (i = 0; i < n; i++) {
-        printf("%5d", a[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("\t %d", a[i]);
     }
-
+    
     l = 0;
     r = n - 1;
-
     qsort(a, l, r);
-
+    
     printf("\nTHE ARRAY ELEMENTS AFTER SORTING: \n");
-    for (i = 0; i < n; i++) {
-        printf("%5d", a[i]);
-    }
+    for (i = 0; i < n; i++)
+        printf("\t %d", a[i]);
+    
+    return 0;
 }
 
-void qsort(int b[], int left, int right) {
-    if (right > left) {
-        int i = left, j = right, pivot = b[left];
+void qsort(int b[], int left, int right)
+{
+    if (left >= right) return;  // Base case for recursion
 
-        while (i < j) {
-            while (b[i] <= pivot && i < right) i++;
-            while (b[j] > pivot) j--;
+    int pivot = b[left];
+    int i = left + 1;
+    int j = right;
 
-            if (i < j) { 
-                // Swap elements
-                int temp = b[i];
-                b[i] = b[j];
-                b[j] = temp;
-            }
+    while (i <= j)
+    {
+        while (i <= right && b[i] <= pivot)
+            i++;
+        
+        while (j > left && b[j] > pivot)
+            j--;
+        
+        if (i < j)
+        {
+            // Swap elements
+            int temp = b[i];
+            b[i] = b[j];
+            b[j] = temp;
         }
-
-        // Swap pivot into correct position
-        int temp = b[left];
-        b[left] = b[j];
-        b[j] = temp;
-
-        // Recursive calls
-        qsort(b, left, j - 1);
-        qsort(b, j + 1, right);
     }
+
+    // Place pivot in correct position
+    int temp = b[left];
+    b[left] = b[j];
+    b[j] = temp;
+
+    // Recursive calls
+    qsort(b, left, j - 1);
+    qsort(b, j + 1, right);
 }
